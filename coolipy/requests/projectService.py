d4e68df27faca50fa7769619c31456c7c810409f
Colin Schmidt <cschmidt@evoila.de>
2025-07-01 13:27:57 +0200

    FIXTURE: added methods to class

--- coolipy/requests/projectService.py
+++ coolipy/requests/projectService.py
@@ -2,14 +2,11 @@ class ProjectService:
     def __init__(self, client_session):
         self._client = client_session
 
+    def list_projects(self):
+        return self._client.request("GET", "/api/v1/projects")
 
-def list_projects(self):
-    return self._client.request("GET", "/api/v1/projects")
+    def get_project(self, uuid):
+        return self._client.request("GET", f"/api/v1/projects/{uuid}")
 
-
-def get_project(self, uuid):
-    return self._client.request("GET", f"/api/v1/projects/{uuid}")
-
-
-def create_project(self, payload):
-    return self._client.request("POST", f"/api/v1/projects", json=payload)
+    def create_project(self, payload):
+        return self._client.request("POST", f"/api/v1/projects", json=payload)
