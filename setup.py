5f8e75c612e1fae997ce37d2d53fb9d2b54d9558
Colin Schmidt <cschmidt@evoila.de>
2025-07-04 13:52:02 +0200

    raised release version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="coolipy",
-    version="0.4.7",
+    version="0.4.8",
     packages=find_packages(),
     install_requires=[
         "requests"
