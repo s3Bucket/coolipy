e711520ece86c93ec75d684d74c74fbdce63f2a8
Colin Schmidt <cschmidt@evoila.de>
2025-07-06 14:59:17 +0200

    adjusted class structure

--- coolipy/requests/__init__.py
+++ coolipy/requests/__init__.py
@@ -2,5 +2,6 @@ from ._client import CoolifySession
 from .projectService import ProjectService
 from .serviceService import ServiceService
 from .resourceService import ResourceService
+from .applicationService import ApplicationService
 
 __all__ = ["CoolifySession"]
